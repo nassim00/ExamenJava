f843c242b452dbf84a93d104c8d59ca073ec29e2
Nassim Filali <nassim.filali@student.ehb.be>
2022-06-13 09:43:51 +0200

    Tabel verhuurder gemaakt met alle datatypes.

--- ExamenJava/src/main/java/be/ehb/ExamenJava/model/auto.java
+++ ExamenJava/src/main/java/be/ehb/ExamenJava/model/auto.java
@@ -25,4 +25,47 @@ public class auto {
     @NotBlank
     private int aantalDeuren;
 
+    public auto() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getChassisNummer() {
+        return chassisNummer;
+    }
+
+    public void setChassisNummer(int chassisNummer) {
+        this.chassisNummer = chassisNummer;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getFabrikant() {
+        return fabrikant;
+    }
+
+    public void setFabrikant(String fabrikant) {
+        this.fabrikant = fabrikant;
+    }
+
+    public int getAantalDeuren() {
+        return aantalDeuren;
+    }
+
+    public void setAantalDeuren(int aantalDeuren) {
+        this.aantalDeuren = aantalDeuren;
+    }
 }
--- ExamenJava/src/main/java/be/ehb/ExamenJava/model/huurder.java
+++ ExamenJava/src/main/java/be/ehb/ExamenJava/model/huurder.java
@@ -27,4 +27,47 @@ public class huurder {
     @NotBlank
     private String email;
 
+
+    public huurder() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+
+    public void setVoornaam(String voornaam) {
+        this.voornaam = voornaam;
+    }
+
+    public String getAchternaam() {
+        return achternaam;
+    }
+
+    public void setAchternaam(String achternaam) {
+        this.achternaam = achternaam;
+    }
+
+    public int getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(int telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
